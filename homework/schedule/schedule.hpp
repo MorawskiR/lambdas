5bfb96e41fe0e1aec87a9e17687c46b75740fb17
MorawskiR <r.morawski92@gmail.com>
2026-02-22 13:34:05 +0100

    Update schedule.hpp

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -1,34 +1,15 @@
 #pragma once
 #include <chrono>
-#include <string>
 #include <functional>
 #include <string>
-#include <iostream>
 #include <thread>
 
+void schedule(std::function<void()> func, std::chrono::seconds duration);
+void schedule(std::function<void(int)> func, std::chrono::seconds duration, int value);
+void schedule(std::function<void(std::string, double)> func, std::chrono::seconds duration, std::string str, double value);
 
-
-void schedule(std::function<void()> func, std::chrono::seconds duration)
-{
-
-    std::this_thread::sleep_for (std::chrono::seconds(duration));
-    func();
-}
-// 
-void schedule(std::function<void(int)> func, std::chrono::seconds duration, int value)
-{
-    std::this_thread::sleep_for (std::chrono::seconds(duration));
-    func(value);
-}
-
-void schedule(std::function<void(std::string a, double b)> func, std::chrono::seconds duration, std::string s, double d)
-{
-    std::this_thread::sleep_for (std::chrono::seconds(duration));
-    func(s, d);
-}
-
-template<typename Function, typename Duration, typename... Argument>
-auto schedule(Function Func , Duration duration, Argument ...arg){
-    std::this_thread::sleep_for(duration);
-    Func(arg...);
+template <typename Func, typename... Args>
+void schedule(Func func, std::chrono::seconds dur, Args... args) {
+    std::this_thread::sleep_for(dur);
+    func(args...);
 }
